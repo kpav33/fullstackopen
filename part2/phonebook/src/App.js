4d3c910164e267ec66ac34baeac510b21bc662c0
Klemen Pavlovič <klemenpavlovic@gmail.com>
2021-12-12 16:07:25 +0100

    Add exercise 2.8. phonebook solution

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -2,17 +2,25 @@ import React, { useState } from "react";
 import "./App.css";
 
 function App() {
-  const [persons, setPersons] = useState([{ name: "Arto Hellas" }]);
+  const [persons, setPersons] = useState([
+    { name: "Arto Hellas", number: "040-123456" },
+  ]);
   const [newName, setNewName] = useState("");
+  const [newNumber, setNewNumber] = useState("");
 
-  function handleInputChange(event) {
+  function handleNameChange(event) {
     setNewName(event.target.value);
+  }
+
+  function handleNumberChange(event) {
+    setNewNumber(event.target.value);
   }
 
   function handleFormSubmit(event) {
     event.preventDefault();
     const newPerson = {
       name: newName,
+      number: newNumber,
     };
 
     // Prevent the user from being able to add names that already exist in the phonebook
@@ -24,13 +32,16 @@ function App() {
     if (preventDuplicates) {
       setPersons((prevPersons) => [...prevPersons, newPerson]);
       setNewName("");
+      setNewNumber("");
     } else {
       alert(`${newName} is already added to phonebook.`);
     }
   }
 
   const personsDisplay = persons.map((person) => (
-    <p key={person.name}>{person.name}</p>
+    <p key={person.name}>
+      {person.name} {person.number}
+    </p>
   ));
 
   return (
@@ -38,15 +49,26 @@ function App() {
       <h2>Phonebook</h2>
       <form onSubmit={handleFormSubmit}>
         <div>
-          name: <input value={newName} onChange={handleInputChange} />
+          name:{" "}
+          <input value={newName} onChange={handleNameChange} name="name" />
         </div>
+        <br />
+        <div>
+          number:{" "}
+          <input
+            value={newNumber}
+            onChange={handleNumberChange}
+            name="number"
+          />
+        </div>
+        <br />
         <div>
           <button type="submit">add</button>
         </div>
       </form>
       <h2>Numbers</h2>
       {personsDisplay}
-      <div>debug: {newName}</div>
+      <div>debug: {newNumber}</div>
     </div>
   );
 }
