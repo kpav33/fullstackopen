0dfc06f6999c036b13722a1bdfd35cb74f0e1693
Klemen Pavlovič <klemenpavlovic@gmail.com>
2021-12-12 15:46:25 +0100

    Add exercise 2.6. phonebook solution

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -5,19 +5,38 @@ function App() {
   const [persons, setPersons] = useState([{ name: "Arto Hellas" }]);
   const [newName, setNewName] = useState("");
 
+  function handleInputChange(event) {
+    setNewName(event.target.value);
+  }
+
+  function handleFormSubmit(event) {
+    event.preventDefault();
+    const newPerson = {
+      name: newName,
+    };
+
+    setPersons((prevPersons) => [...prevPersons, newPerson]);
+    setNewName("");
+  }
+
+  const personsDisplay = persons.map((person) => (
+    <p key={person.name}>{person.name}</p>
+  ));
+
   return (
     <div>
       <h2>Phonebook</h2>
-      <form>
+      <form onSubmit={handleFormSubmit}>
         <div>
-          name: <input />
+          name: <input value={newName} onChange={handleInputChange} />
         </div>
         <div>
           <button type="submit">add</button>
         </div>
       </form>
       <h2>Numbers</h2>
-      ...
+      {personsDisplay}
+      <div>debug: {newName}</div>
     </div>
   );
 }
