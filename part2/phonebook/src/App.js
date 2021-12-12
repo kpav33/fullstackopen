c63d4041602674dd207360a618701ab3d5252705
Klemen Pavlovič <klemenpavlovic@gmail.com>
2021-12-12 16:27:58 +0100

    Add exercise 2.9. phonebook solution

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -3,10 +3,17 @@ import "./App.css";
 
 function App() {
   const [persons, setPersons] = useState([
-    { name: "Arto Hellas", number: "040-123456" },
+    { name: "Arto Hellas", number: "040-123456", id: 1 },
+    { name: "Ada Lovelace", number: "39-44-5323523", id: 2 },
+    { name: "Dan Abramov", number: "12-43-234345", id: 3 },
+    { name: "Mary Poppendieck", number: "39-23-6423122", id: 4 },
   ]);
   const [newName, setNewName] = useState("");
   const [newNumber, setNewNumber] = useState("");
+  const [filter, setFilter] = useState("");
+
+  // console.log(persons);
+  // console.log(filter);
 
   function handleNameChange(event) {
     setNewName(event.target.value);
@@ -16,11 +23,21 @@ function App() {
     setNewNumber(event.target.value);
   }
 
+  function handleFilterChange(event) {
+    setFilter(event.target.value);
+  }
+
   function handleFormSubmit(event) {
     event.preventDefault();
+    if (newName === "" || newNumber === "") {
+      alert("Please fill all input fields!");
+      return;
+    }
+
     const newPerson = {
       name: newName,
       number: newNumber,
+      id: persons.length + 1,
     };
 
     // Prevent the user from being able to add names that already exist in the phonebook
@@ -38,15 +55,30 @@ function App() {
     }
   }
 
-  const personsDisplay = persons.map((person) => (
-    <p key={person.name}>
-      {person.name} {person.number}
-    </p>
-  ));
+  const personsDisplay = persons
+    .filter((person) =>
+      person.name.toLowerCase().includes(filter.toLowerCase())
+    )
+    .map((person) => (
+      <p key={person.id}>
+        {person.name} {person.number}
+      </p>
+    ));
+
+  // const personsDisplayFiltered = persons.filter((person) =>
+  //   person.name.toLowerCase().includes(filter.toLowerCase())
+  // );
+
+  // console.log(personsDisplayFiltered);
 
   return (
     <div>
       <h2>Phonebook</h2>
+      <div>
+        filter shown with{" "}
+        <input value={filter} onChange={handleFilterChange} name="filter" />
+      </div>
+      <h2>Add a new</h2>
       <form onSubmit={handleFormSubmit}>
         <div>
           name:{" "}
