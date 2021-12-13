19ec55cc102d79e962faf95c8d50478a5d1bd360
Klemen Pavlovič <klemenpavlovic@gmail.com>
2021-12-13 13:55:15 +0100

    Add part2 exercise 2.11. solution

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -1,19 +1,25 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 import "./App.css";
 import Filter from "./components/Filter";
 import PersonForm from "./components/PersonForm";
 import Persons from "./components/Persons";
 
 function App() {
-  const [persons, setPersons] = useState([
-    { name: "Arto Hellas", number: "040-123456", id: 1 },
-    { name: "Ada Lovelace", number: "39-44-5323523", id: 2 },
-    { name: "Dan Abramov", number: "12-43-234345", id: 3 },
-    { name: "Mary Poppendieck", number: "39-23-6423122", id: 4 },
-  ]);
+  const [persons, setPersons] = useState([]);
   const [newName, setNewName] = useState("");
   const [newNumber, setNewNumber] = useState("");
   const [filter, setFilter] = useState("");
+
+  // Get data from JSON server and store it into state
+  useEffect(() => {
+    // console.log("effect");
+    axios.get("http://localhost:3001/persons").then((response) => {
+      // console.log("Fulfilled");
+      setPersons(response.data);
+    });
+  }, []);
+  // console.log("render", persons.length, "persons");
 
   function handleNameChange(event) {
     setNewName(event.target.value);
