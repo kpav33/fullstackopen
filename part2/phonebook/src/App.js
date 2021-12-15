658897d70a2d7678113d56e983a880931683f37b
Klemen Pavlovič <klemenpavlovic@gmail.com>
2021-12-15 13:54:50 +0100

    Add part2 exercise 2.20. solution

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from "react";
 import personService from "./services/persons";
 import "./App.css";
 import SuccessNotification from "./components/SuccessNotification";
+import ErrorNotification from "./components/ErrorNotification";
 import Filter from "./components/Filter";
 import PersonForm from "./components/PersonForm";
 import Persons from "./components/Persons";
@@ -13,14 +14,26 @@ function App() {
   const [newNumber, setNewNumber] = useState("");
   const [filter, setFilter] = useState("");
   const [successNotification, setSuccessNotification] = useState(null);
+  const [errorNotification, setErrorNotification] = useState(null);
 
   // Get data from JSON server and store it into state
   useEffect(() => {
     // console.log("effect");
-    personService.getAll().then((initalPersons) => {
-      // console.log("Fulfilled");
-      setPersons(initalPersons);
-    });
+    personService
+      .getAll()
+      .then((initalPersons) => {
+        // console.log("Fulfilled");
+        setPersons(initalPersons);
+      })
+      .catch((error) => {
+        // Show error message on error
+        setErrorNotification(
+          `There was an error when trying to fetch data from the server. Try again later.`
+        );
+        setTimeout(() => {
+          setErrorNotification(null);
+        }, 3000);
+      });
   }, []);
   // console.log("render", persons.length, "persons");
 
@@ -39,11 +52,22 @@ function App() {
   // Handle deleting a person
   function handleDeletePerson(id, name) {
     if (window.confirm(`Delete ${name}?`)) {
-      personService.deletePerson(id).then(() => {
-        setPersons((prevPersons) =>
-          prevPersons.filter((person) => person.id !== id)
-        );
-      });
+      personService
+        .deletePerson(id)
+        .then(() => {
+          setPersons((prevPersons) =>
+            prevPersons.filter((person) => person.id !== id)
+          );
+        })
+        .catch((error) => {
+          // Show error message on error
+          setErrorNotification(
+            `There was an error when trying to delete ${name} from the server. Try again later.`
+          );
+          setTimeout(() => {
+            setErrorNotification(null);
+          }, 3000);
+        });
     }
   }
 
@@ -69,23 +93,35 @@ function App() {
 
     if (preventDuplicates) {
       // Add new person to the server
-      personService.create(newPerson).then((newNote) => {
-        // Show notification on added person
-        setSuccessNotification(`Added ${newNote.name}`);
-        setTimeout(() => {
-          setSuccessNotification(null);
-        }, 3000);
-        // Display updated persons and reset state
-        setNewName("");
-        setNewNumber("");
-        setPersons((prevPersons) => [...prevPersons, newNote]);
-      });
+      personService
+        .create(newPerson)
+        .then((newNote) => {
+          // Show notification on added person
+          setSuccessNotification(`Added ${newNote.name}`);
+          setTimeout(() => {
+            setSuccessNotification(null);
+          }, 3000);
+          // Display updated persons and reset state
+          setNewName("");
+          setNewNumber("");
+          setPersons((prevPersons) => [...prevPersons, newNote]);
+        })
+        .catch((error) => {
+          // Show error message on error
+          setErrorNotification(
+            `There was an error when adding ${newPerson.name} to the server. Try again later.`
+          );
+          setTimeout(() => {
+            setErrorNotification(null);
+          }, 3000);
+        });
     } else {
       if (
         window.confirm(
           `${newName} is already added to the phonebook, replace the old number with the new one?`
         )
       ) {
+        // Update the phone number for selected person
         const findPerson = persons.find(
           (person) => person.name.toLowerCase() === newPerson.name.toLowerCase()
         );
@@ -98,12 +134,21 @@ function App() {
             setTimeout(() => {
               setSuccessNotification(null);
             }, 3000);
-            // Display updated values
+            // Display updated persons
             setPersons((prevPersons) =>
               prevPersons.map((person) =>
                 person.id !== findPerson.id ? person : updatedPerson
               )
             );
+          })
+          .catch((error) => {
+            // Show error message on error
+            setErrorNotification(
+              `Information of ${changedPerson.name} has already been removed from server.`
+            );
+            setTimeout(() => {
+              setErrorNotification(null);
+            }, 3000);
           });
       } else {
         alert(`${newName} is already added to phonebook.`);
@@ -115,6 +160,7 @@ function App() {
     <div>
       <h2>Phonebook</h2>
       <SuccessNotification message={successNotification} />
+      <ErrorNotification message={errorNotification} />
       <Filter filter={filter} handleFilterChange={handleFilterChange} />
 
       <h2>Add a new</h2>
