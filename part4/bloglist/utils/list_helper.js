4f4277730946d6536fe88dc383a7ef21d5ca480e
Klemen Pavlovič <klemenpavlovic@gmail.com>
2021-12-27 16:09:24 +0100

    Add part4 exercise 4.4. solution

--- part4/bloglist/utils/list_helper.js
+++ part4/bloglist/utils/list_helper.js
@@ -3,4 +3,8 @@ const dummy = (blogs) => {
   return 1;
 };
 
-module.exports = { dummy };
+const totalLikes = (blogs) => {
+  return blogs.reduce((a, b) => a + b.likes, 0);
+};
+
+module.exports = { dummy, totalLikes };
