774d8d18f4abe223e541c294efd7e9f3eed73a03
Klemen Pavlovič <klemenpavlovic@gmail.com>
2021-12-30 15:06:46 +0100

    Add part4 exercise 4.15. solution

--- part4/bloglist/tests/test_helper.js
+++ part4/bloglist/tests/test_helper.js
@@ -1,4 +1,5 @@
 const Blog = require("../models/blog");
+const User = require("../models/user");
 
 const initialBlogs = [
   {
@@ -15,7 +16,6 @@ const initialBlogs = [
   },
 ];
 
-// Remove?
 const nonExistingId = async () => {
   const blog = new Blog({
     title: "willremovethissoon",
@@ -34,8 +34,14 @@ const blogsInDb = async () => {
   return blogs.map((blog) => blog.toJSON());
 };
 
+const usersInDb = async () => {
+  const users = await User.find({});
+  return users.map((user) => user.toJSON());
+};
+
 module.exports = {
   initialBlogs,
   nonExistingId,
   blogsInDb,
+  usersInDb,
 };
