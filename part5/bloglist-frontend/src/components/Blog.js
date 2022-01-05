63542da79f2b3b629ba24863dcd6ac3a5ccb27e1
Klemen Pavlovič <klemenpavlovic@gmail.com>
2022-01-05 11:52:30 +0100

    Add part5 exercise 5.7. solution

--- part5/bloglist-frontend/src/components/Blog.js
+++ part5/bloglist-frontend/src/components/Blog.js
@@ -1,8 +1,50 @@
-import React from "react";
-const Blog = ({ blog }) => (
-  <div>
-    {blog.title} {blog.author}
-  </div>
-);
+import React, { useState } from "react";
+const Blog = ({ blog }) => {
+  const [showDetails, setShowDetails] = useState(false);
+
+  const handleShowDetails = () => {
+    setShowDetails((prevState) => !prevState);
+  };
+
+  const blogStyle = {
+    paddingTop: 10,
+    paddingBottom: 10,
+    paddingLeft: 2,
+    border: "solid",
+    borderWidth: 1,
+    marginBottom: 5,
+  };
+
+  console.log(blog);
+
+  return (
+    <div style={blogStyle}>
+      <div>
+        {blog.title} {blog.author}{" "}
+        <button onClick={handleShowDetails}>
+          {showDetails ? "hide" : "view"}
+        </button>
+      </div>
+      {showDetails && (
+        <div>
+          <p>
+            <strong>URL: </strong>
+            {blog.url}
+          </p>
+          <div>
+            <p>
+              <strong>Likes</strong> {blog.likes}
+            </p>
+            <button>Like</button>
+          </div>
+          <p>
+            <strong>Author: </strong>
+            {blog.author}
+          </p>
+        </div>
+      )}
+    </div>
+  );
+};
 
 export default Blog;
