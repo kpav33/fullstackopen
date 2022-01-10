d405ac082cf93a3f8637d1eea8a808143798927a
Klemen Pavlovič <klemenpavlovic@gmail.com>
2022-01-10 13:57:54 +0100

    Add part5 exercise 5.18. solution

--- part5/bloglist-frontend/cypress/integration/blog_app.spec.js
+++ part5/bloglist-frontend/cypress/integration/blog_app.spec.js
@@ -14,7 +14,7 @@ describe("Blog app", function () {
   //     cy.contains("log in to application");
   //   });
 
-  it("login form is shown", function () {
+  it("Login form is shown", function () {
     cy.contains("log in to application");
     cy.contains("username");
     cy.contains("password");
@@ -26,4 +26,24 @@ describe("Blog app", function () {
   //     cy.get("input[name='Password']").type("password");
   //     cy.get("#login-button");
   //   });
+
+  describe("Login", function () {
+    it("succeeds with correct credentials", function () {
+      cy.get("input[name='Username']").type("John");
+      cy.get("input[name='Password']").type("password");
+      cy.get("#login-button").click();
+
+      cy.contains("John Smith logged in");
+    });
+
+    it("fails with wrong credentials", function () {
+      cy.get("input[name='Username']").type("John");
+      cy.get("input[name='Password']").type("wrong-password");
+      cy.get("#login-button").click();
+
+      cy.get(".error").should("contain", "wrong username or password");
+      cy.get(".error").should("have.css", "color", "rgb(255, 0, 0)");
+      cy.get(".error").should("have.css", "border-style", "solid");
+    });
+  });
 });
