69fbdb054d4ea925777af0105b5d89de6fee54c7
Klemen Pavlovič <klemenpavlovic@gmail.com>
2022-01-16 15:37:25 +0100

    Add part6 exercise 6.13. solution

--- part6/redux-anecdotes/src/App.js
+++ part6/redux-anecdotes/src/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect } from "react";
 import AnecdoteForm from "./components/AnecdoteForm";
 import AnecdoteList from "./components/AnecdoteList";
 import Notification from "./components/Notification";
@@ -8,9 +8,16 @@ import {
   showNotification,
   hideNotification,
 } from "./reducers/notificationReducer";
+import anecdoteService from "./services/anecdotes";
+import { initializeAnecdotes } from "./reducers/anecdoteReducer";
 
 const App = () => {
   const dispatch = useDispatch();
+  useEffect(() => {
+    anecdoteService
+      .getAll()
+      .then((anecdotes) => dispatch(initializeAnecdotes(anecdotes)));
+  }, [dispatch]);
 
   // Create a notification
   const addNotification = (message) => {
--- part6/redux-anecdotes/src/store.js
+++ part6/redux-anecdotes/src/store.js
@@ -4,6 +4,7 @@ import anecdoteReducer from "./reducers/anecdoteReducer";
 import notificationReducer from "./reducers/notificationReducer";
 import filterReducer from "./reducers/filterReducer";
 // import { notificationChange } from "./reducers/notificationReducer";
+// import anecdoteService from "./services/anecdotes";
 
 const reducer = combineReducers({
   anecdotes: anecdoteReducer,
@@ -12,6 +13,20 @@ const reducer = combineReducers({
 });
 
 const store = createStore(reducer, composeWithDevTools());
+
+// anecdoteService
+//   .getAll()
+//   .then((anecdotes) =>
+//     anecdotes.forEach((anecdote) =>
+//       store.dispatch({ type: "NEW_ANECDOTE", data: anecdote })
+//     )
+//   );
+
+// Same as above but simplified
+// Moved to App.js
+// anecdoteService
+//   .getAll()
+//   .then((anecdotes) => store.dispatch(initializeAnecdotes(anecdotes)));
 
 // console.log(store.getState());
 // store.dispatch(notificationChange("OK"));
