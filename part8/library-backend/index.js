469c601e53ef842f7b46f81f22746e334ae2457f
Klemen Pavlovič <klemenpavlovic@gmail.com>
2022-02-01 15:06:13 +0100

    Add part8 exercise 8.5. solution

--- part8/library-backend/index.js
+++ part8/library-backend/index.js
@@ -97,7 +97,7 @@ const typeDefs = gql`
   type Query {
     bookCount: Int!
     authorCount: Int!
-    allBooks(author: String): [Book!]!
+    allBooks(author: String, genre: String): [Book!]!
     allAuthors: [Author!]!
   }
 `;
@@ -107,10 +107,22 @@ const resolvers = {
     bookCount: () => books.length,
     authorCount: () => authors.length,
     allBooks: (root, args) => {
-      if (!args.author) {
-        return books;
+      if (args.author) {
+        if (args.genre) {
+          return books.filter(
+            (book) =>
+              book.author === args.author && book.genres.includes(args.genre)
+          );
+          // .filter((book) => book.genres.includes(args.genre));
+        }
+        return books.filter((book) => book.author === args.author);
       }
-      return books.filter((book) => book.author === args.author);
+
+      if (args.genre) {
+        return books.filter((book) => book.genres.includes(args.genre));
+      }
+
+      return books;
     },
     // allAuthors: () =>
     //   authors.map((author) => {
