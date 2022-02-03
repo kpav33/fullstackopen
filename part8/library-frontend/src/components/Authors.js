028187147cbb55ee61c6b1bb60e57b34d94e04eb
Klemen Pavlovič <klemenpavlovic@gmail.com>
2022-02-03 10:56:00 +0100

    Add part8 exercise 8.11. solution

--- part8/library-frontend/src/components/Authors.js
+++ part8/library-frontend/src/components/Authors.js
@@ -1,9 +1,22 @@
-import React from "react";
-import { useQuery } from "@apollo/client";
-import { ALL_AUTHORS } from "../queries";
+import React, { useState, useEffect } from "react";
+import { useQuery, useMutation } from "@apollo/client";
+import { ALL_AUTHORS, EDIT_AUTHOR } from "../queries";
 
-const Authors = ({ show }) => {
+const Authors = ({ show, setError }) => {
   const result = useQuery(ALL_AUTHORS);
+  const [name, setName] = useState("");
+  const [setBornTo, setBornYear] = useState("");
+
+  const [changeAuthor, resultChange] = useMutation(EDIT_AUTHOR, {
+    refetchQueries: [{ query: ALL_AUTHORS }],
+  });
+
+  useEffect(() => {
+    if (resultChange.data && resultChange.data.editAuthor === null) {
+      setError("person not found");
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [resultChange.data]);
 
   if (!show) {
     return null;
@@ -12,6 +25,14 @@ const Authors = ({ show }) => {
   if (result.loading) {
     return <div>loading...</div>;
   }
+
+  const submit = (event) => {
+    event.preventDefault();
+    changeAuthor({ variables: { name, setBornTo } });
+
+    setName("");
+    setBornYear("");
+  };
 
   return (
     <div>
@@ -32,6 +53,24 @@ const Authors = ({ show }) => {
           ))}
         </tbody>
       </table>
+      <h3>Set birthyear</h3>
+      <form onSubmit={submit}>
+        <div>
+          name
+          <input
+            value={name}
+            onChange={({ target }) => setName(target.value)}
+          />
+        </div>
+        <div>
+          born
+          <input
+            value={setBornTo}
+            onChange={({ target }) => setBornYear(parseInt(target.value))}
+          />
+        </div>
+        <button type="submit">update author</button>
+      </form>
     </div>
   );
 };
